eca03a06967277497e7d57b583b23560a41c9a40
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-19 19:13:17 -0600

    Add index.css import to main.jsx for styling

--- memory_game/src/main.jsx
+++ memory_game/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // Asegúrate de que la ruta sea correcta
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
